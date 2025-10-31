97d19faea976f0339f76bddcb619d99bc67fc2d7
Zhou <xzhou473@cos-4a11216.cos.gatech.edu>
2025-10-31 17:31:02 -0400

    clean the example run

--- Java_real/examplerun/download.sh
+++
@@ -1,5 +0,0 @@
-rsync -ave ssh -hPz xzhou473@login-phoenix-rh9-3.pace.gatech.edu:/storage/home/hcoda1/9/xzhou473/scratch/LLMagent_test/output/20160910/ichthyop-3.3.17-LLM.jar .
-rsync -ave ssh -hPz xzhou473@login-phoenix-rh9-3.pace.gatech.edu:/storage/home/hcoda1/9/xzhou473/scratch/LLMagent_test/output/20160910/prompt.txt .
-rsync -ave ssh -hPz xzhou473@login-phoenix-rh9-3.pace.gatech.edu:/storage/home/hcoda1/9/xzhou473/scratch/LLMagent_test/output/20160910/ptrajstates_config.json .
-rsync -ave ssh -hPz xzhou473@login-phoenix-rh9-3.pace.gatech.edu:/storage/home/hcoda1/9/xzhou473/scratch/LLMagent_test/output/20160910/config.json .
-rsync -ave ssh -hPz xzhou473@login-phoenix-rh9-3.pace.gatech.edu:/storage/home/hcoda1/9/xzhou473/scratch/LLMagent_test/output/20160910/RS_20160910.xml example.xml
